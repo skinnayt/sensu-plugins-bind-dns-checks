aae7f2e69659c769599fbd35a079255b7527d81f
Trevor Schellhorn <trevor@trevorschellhorn.ca>
2024-02-08 22:40:39 -0700

    Add testing for the different types and groups of arguments that can be set

--- main_test.go
+++ main_test.go
@@ -1,8 +1,85 @@
 package main
 
 import (
+	"os"
+	"plugin"
 	"testing"
+
+	"github.com/smartystreets/assertions/assert"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
+func TestMain(m *testing.M) {
+	exitVal := m.Run()
+	os.Exit(exitVal)
+}
+
+func TestCheckArgs(t *testing.T) {
+	// Arguments that should be checked
+	// The bind DNS server will listen on an ip address and port for the statistics channel
+	// The bind DNS server might be configured with XML or JSON for returning the statistics
+	// THe bind DNS server can also dump the statistics to a file
+
+	// Setup the assert object for running the tests
+	assert := assert.New(t)
+
+	// Make sure we can only pass in file, xml or json for the statistics format
+	// When the format is file, we need to specify the file path
+	plugin.StatisticsFormat = "file"
+	plugin.StatisticsFilePath = "/var/named/named.stats"
+	ok, err := checkArgs(nil)
+	assert.Equal(ok, 0)
+	assert.NoError(err)
+
+	// Make sure we get an error when the format is not file, xml or json
+	plugin.StatisticsFormat = "invalid"
+	ok, err = checkArgs(nil)
+	assert.Equal(ok, 3)
+	assert.Error(err)
+
+	// Make sure we get an error when format is file but not filepath set
+	plugin.StatisticsFormat = "file"
+	plugin.StatisticsFilePath = ""
+	ok, err = checkArgs(nil)
+	assert.Equal(ok, 3)
+	assert.Error(err)
+
+	// For both XML and JSON, we don't need to specify a file path
+	// But we need to specify the ip address and port
+	plugin.StatisticsFormat = "xml"
+	plugin.StatisticsFilePath = ""
+	plugin.StatisticsIP = "127.0.0.1"
+	plugin.StatisticsPort = 8053
+	ok, err = checkArgs(nil)
+	assert.Equal(ok, 0)
+	assert.NoError(err)
+
+	// Set bad ip address
+	plugin.StatisticsFormat = "xml"
+	plugin.StatisticsFilePath = ""
+	plugin.StatisticsIP = "bad.ip.address"
+	plugin.StatisticsPort = 8053
+	ok, err = checkArgs(nil)
+	assert.Equal(ok, 3)
+	assert.Error(err)
+
+	// Set bad port
+	plugin.StatisticsFormat = "xml"
+	plugin.StatisticsFilePath = ""
+	plugin.StatisticsIP = "127.0.0.1"
+	plugin.StatisticsPort = 65536
+	ok, err = checkArgs(nil)
+	assert.Equal(ok, 3)
+	assert.Error(err)
+
+	plugin.StatisticsFormat = "json"
+	plugin.StatisticsFilePath = ""
+	plugin.StatisticsIP = "127.0.0.1"
+	plugin.StatisticsPort = 8053
+	ok, err = checkArgs(nil)
+	assert.Equal(ok, 0)
+	assert.NoError(err)
+}
+
+func TestExecuteCheck(t *testing.T) {
 }
